d360d214cda272cc4fde2df44e79a9b95cf44c11
ptrmagaga <opiniobox@gmail.com>
2025-04-28 09:27:07 +1400

    Restructure project for cleaner deployment

--- health_system/core/serializers.py
+++ health_system/core/serializers.py
@@ -1,32 +1,37 @@
 from rest_framework import serializers
-from health_system.core.models import Client,HealthProgram,Enrollment
+from health_system.core.models import Client, HealthProgram, Enrollment
 
 class HealthProgramSerializer(serializers.ModelSerializer):
+    # To include the related health programs, as it's a many-to-many field
     enrolled_programs = serializers.PrimaryKeyRelatedField(queryset=HealthProgram.objects.all(), many=True)
 
-
     class Meta:
-        model=HealthProgram
-        fields='__all__'
+        model = HealthProgram
+        fields = '__all__'
 
 
 class ClientSerializer(serializers.ModelSerializer):
+    # Simple serializer to represent the Client model
     class Meta:
-        model= Client
-        fields='__all__'
+        model = Client
+        fields = '__all__'
 
 
 class EnrollmentSerializer(serializers.ModelSerializer):
+    # Serializer for Enrollment model
     class Meta:
-        model=Enrollment
-        fields='__all__'
+        model = Enrollment
+        fields = '__all__'
+
 
 class ClientProfileSerializer(serializers.ModelSerializer):
-    enrolled_programs=serializers.SerializerMethodField()
+    # This serializer will represent a specific Client's profile
+    enrolled_programs = serializers.SerializerMethodField()
 
     class Meta:
-        model=Client
-        fields =['id','full_name','age','contact_number','enrolled_programs']
+        model = Client
+        fields = ['id', 'full_name', 'age', 'contact_number', 'enrolled_programs']
 
-    def get_enrolled_programs(self,obj):
+    def get_enrolled_programs(self, obj):
+        # Here, we return the program names associated with this client
         return [en.program.name for en in obj.enrollments.all()]
--- health_system/core/views.py
+++ health_system/core/views.py
@@ -1,22 +1,48 @@
-from rest_framework import viewsets,generics,filters
-from health_system.core.models import Client,HealthProgram,Enrollment
-from .serializers import ClientSerializer,HealthProgramSerializer,EnrollmentSerializer
+from rest_framework import viewsets, filters
+from rest_framework.response import Response
+from .models import Client, HealthProgram, Enrollment
+from .serializers import ClientSerializer, HealthProgramSerializer, EnrollmentSerializer
 
+# ViewSet for managing Clients
 class ClientViewSet(viewsets.ModelViewSet):
-    queryset= Client.objects.all()
-    serializer_class=ClientSerializer
-    filter_backends=[filters.SearchFilter]
-    search_fields=['full_name','contact_number']
+    """
+    ViewSet for managing clients.
+    """
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['full_name', 'contact_number']
 
+# ViewSet for managing Health Programs
 class HealthProgramViewSet(viewsets.ModelViewSet):
-    queryset=HealthProgram.objects.all()
-    serializer_class=HealthProgramSerializer
+    """
+    ViewSet for managing health programs.
+    """
+    queryset = HealthProgram.objects.all()
+    serializer_class = HealthProgramSerializer
 
+# ViewSet for managing Enrollments
 class EnrollmentViewSet(viewsets.ModelViewSet):
-    queryset=Enrollment.objects.all()
-    serializer_class=EnrollmentSerializer
+    """
+    ViewSet for managing enrollments.
+    """
+    queryset = Enrollment.objects.all()
+    serializer_class = EnrollmentSerializer
 
-class ClientProfileViewSet(viewsets.ModelViewSet):
-    queryset=Client.objects.all()
-    serializer_class=ClientSerializer
+# Custom ViewSet for retrieving a client's profile
+class ClientProfileViewSet(viewsets.ViewSet):
+    """
+    A ViewSet to retrieve the profile of a client.
+    """
 
+    def retrieve(self, request, pk=None):
+        """
+        Retrieve the profile of a client by their ID.
+        """
+        try:
+            client = Client.objects.get(pk=pk)
+        except Client.DoesNotExist:
+            return Response({'detail': 'Client not found'}, status=404)
+
+        serializer = ClientSerializer(client)
+        return Response(serializer.data)
