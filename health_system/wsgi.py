7919a203b1ab113e921cc00f3f2ce3d0c7004e69
ptrmagaga <opiniobox@gmail.com>
2025-04-28 09:00:17 +1400

    Restructure project for cleaner deployment

--- health_system/wsgi.py
+++ health_system/wsgi.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), 'health_system'))
 
 from django.core.wsgi import get_wsgi_application
 
